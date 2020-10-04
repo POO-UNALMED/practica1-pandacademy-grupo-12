cb1d99050f9075a650488386ae8a341cc29f51f4
Jurregoz <jonathanurrego@hotmail.com>
2020-10-04 13:05:22 -0500

    Métodos Estudian Persona y Profesor

--- PANDACADEMY/src/gestorAplicacion/Profesor.java
+++ PANDACADEMY/src/gestorAplicacion/Profesor.java
@@ -7,4 +7,8 @@ public class Profesor {
 	private List<Horario> asesoria;
 	private Asignatura asignatura;
 	public String detalles;
+	
+	public void setAsignatura(String asignatura) {
+		this.asignatura = asignatura;
+	}
 }
