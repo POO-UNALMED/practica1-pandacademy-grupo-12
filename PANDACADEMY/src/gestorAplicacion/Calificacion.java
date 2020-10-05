e057df6f1e3c9d3252f86bfbc52cf14261f1bfbf
Cristian Londoño Franco <clondonof@unal.edu.co>
2020-10-05 10:18:27 -0500

    cambios en Calificaciones

--- PANDACADEMY/src/gestorAplicacion/Calificacion.java
+++ PANDACADEMY/src/gestorAplicacion/Calificacion.java
@@ -1,8 +1,9 @@
 package gestorAplicacion;
+import Nota.*;
 import java.util.*;
 public class Calificacion {
-	private Estudiante estudiante;
-	public static List<Nota> Nota = new ArrayList<Nota>();
+
+	private ArrayList<Nota> notas = new ArrayList<Nota>();
 	private Asignatura asignatura;
 	
 	public Calificacion() {
@@ -16,22 +17,16 @@ public class Calificacion {
 	public Nota agregarNota(Nota nota) {
 		Nota.add(nota);
 	}
-	public Estudiante getEstudiante() {
-		return this.estudiante;
-	}
-	public void setEstudiante(Estudiante estudiante) {
-		this.estudiante = estudiante;
-	}
 	public Asignatura getAsignatura() {
 		return this.asignatura;
 	}
 	public void setAsignatura(Asignatura asignatura) {
 		this.asignatura = asignatura;
 	} 
-	public static double promedio() {
-		double cont=0;
-		for (int i=0;i<Nota.size();i++) {
-			cont=cont + (Nota.get(i).getNota()*Nota.get(i).getPorcentaje()) ;
+	public float promedio() {
+		float cont=0;
+		for (int i=0;i<notas.size();i++) {
+			cont=cont + (notas.get(i).getNota()*notas.get(i).getPorcentaje()) ;
 		}
 		return cont;
 	}
