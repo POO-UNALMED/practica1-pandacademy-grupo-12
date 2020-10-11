aba236687ed28cb2ded0b685f3cd33e820e50f6b
Cristian Londoño Franco <clondonof@unal.edu.co>
2020-10-11 12:19:56 -0500

    IMPLEMENTACION DEL BUFFEREDREADER

--- PANDACADEMY/src/uiMain/Interface.java
+++ PANDACADEMY/src/uiMain/Interface.java
@@ -1,36 +1,32 @@
 package src.uiMain;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import src.gestorAplicacion.*;
 
 public class Interface {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     Estudiante e = new Estudiante();
-    Scanner sc = new Scanner(System.in); // lector que se usara
-    System.out.println("?????PANDACADEMY?????");
-    // aca podemos usar un metodo para ver si existen datos guardados para omitir lo que sigue
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("=====PANDACADEMY=====");
+    // aca podemos usar un metodo para ver si existen datos guardados para omitir lo
+    // que sigue
     System.out.println("BIENVENIDO \n POR FAVOR INGRESE SU NOMBRE: ");
-    e.setNombre(sc.nextLine());
+    String entrada = br.readLine();
+    e.setNombre(entrada);
     System.out.println("INGRESE SU DOCUMENTO DE IDENTIDAD(OPCIONAL): ");
-    e.setDni(sc.nextLong());
-    System.out.println("INGRESE SU CORREO: ");
-    e.setCorreo(sc.nextLine()); //el usuario ingresa sus datos personales
-
-      //menu principal de la aplicacion
-    System.out.println(
-    e.getNombre().toUpperCase() +
-    "\n ?????MENU PRINCIPAL????? \n [0] PERFIL \n [1] ASIGNATURAS \n [2] NOTAS \n [3] HORARIO \n [4] PROFESORES \n [5] SALIR");
-      int comando = sc.nextInt();
-      while (true) {
-        switch (comando) {
-          case 1:
-            System.out.println("????PERFIL???? \n [0] EDITAR PERFIL \n [1] MOSTRAR MI PERFIL");
-            break;
-        
-          default:
-            break;
-        }
-      }
+    entrada = br.readLine();
+    if (!entrada.equals("")) {
+      e.setDni(Long.parseLong(entrada));
     }
+    System.out.println("INGRESE SU CORREO(OPCIONAL): ");
+    e.setCorreo(br.readLine()); // el usuario ingresa sus datos personales
+
+    // menu principal de la aplicacion
+    System.out.println(" \n" + e.getNombre().toUpperCase()
+        + "\n=====MENU PRINCIPAL===== \n [0] PERFIL \n [1] ASIGNATURAS \n [2] NOTAS \n [3] HORARIO \n [4] PROFESORES \n [5] SALIR");
+    int comando = Integer.parseInt(br.readLine());
+  }
 }
