ad845b7a6549f4dd741d7c3caf9e1dc80e407ee6
Sixa <mrjoer22@gmail.com>
2020-10-19 09:22:47 -0500

    Creación clase Serialization

---
+++ PANDACADEMY/src/BaseDatos/Serialization.java
@@ -0,0 +1,15 @@
+package BaseDatos;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Serialization {
+	
+	static File Archivo = new File("");
+	ArrayList<ArrayList> objetos = new ArrayList<>();
+	
+	public static void ejecutar() {
+		
+	}
+	
+}
