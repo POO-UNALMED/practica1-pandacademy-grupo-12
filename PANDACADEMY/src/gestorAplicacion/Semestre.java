d722f5a1254309a7822c8b64e7f3c3a42d02a274
Cristian Londoño Franco <clondonof@unal.edu.co>
2020-10-27 14:38:48 -0500

    clase semestre terminada

--- PANDACADEMY/src/gestorAplicacion/Semestre.java
+++ PANDACADEMY/src/gestorAplicacion/Semestre.java
@@ -3,31 +3,67 @@ package gestorAplicacion;
 import java.util.ArrayList;
 
 public class Semestre {
-	
+
 	private String nombre;
 	private ArrayList<Asignatura> asignaturas = new ArrayList<>();
-	
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
-	public  float promedioSemetre(){
-		
+
+	public float promedioSemetre() {
+		float p = 0;
+		for (int i = 0; i < asignaturas.size(); i++) {
+			Asignatura m = asignaturas.get(i);
+			p = p + (m.promedioAsignatura() * m.getCreditos());
+		}
+		return p;
 	}
-	
-	public int creditosAprobados(){
-		
+
+	public int creditosAprobados() {
+		int total = 0;
+		for (int i = 0; i < asignaturas.size(); i++) {
+			Asignatura m = asignaturas.get(i);
+			if (m.estadoAsignatura().indexOf("Aprobada") == 0) {
+				total = total + m.getCreditos();
+			}
+		}
+		return total;
+
 	}
-	
+
 	public int totalCreditos() {
-		
+		int total = 0;
+		for (int i = 0; i < asignaturas.size(); i++) {
+			total = total + asignaturas.get(i).getCreditos();
+		}
+		return total;
 	}
-	
-	public void addAsignatura() {
-		
+
+	public void addAsignatura(Asignatura asignatura) {
+		this.asignaturas.add(asignatura);
+	}
+
+	public void removeAsignatura(Asignatura asignatura) {
+		this.asignaturas.remove(asignatura);
+	}
+
+	public Asignatura getAsignatura(int index) {
+		return asignaturas.get(index);
+	}
+
+	public Asignatura getAsignatura(String nombre) {
+		for (int i = 0; i < asignaturas.size(); i++) {
+			Asignatura m = asignaturas.get(i);
+			if (m.getNombre().equalsIgnoreCase(nombre)) {
+				return m;
+			}
+		}
+		return null;
 	}
 
 }
